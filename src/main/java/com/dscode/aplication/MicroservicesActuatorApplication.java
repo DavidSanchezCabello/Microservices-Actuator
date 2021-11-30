b78909871ca3f2f8d836878d6da019f0355451ff
David <david.cabello.info@gmail.com>
2021-11-30 14:32:17 +0100

    first commit

---
+++ src/main/java/com/dscode/aplication/MicroservicesActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.dscode.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesActuatorApplication.class, args);
+	}
+
+}
